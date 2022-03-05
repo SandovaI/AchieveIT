59c92817faba2636799adb0ccbc805669cae4015
SandovaI <calebsandoval901@gmail.com>
2022-03-05 15:49:09 -0800

    navbar

--- src/components/Nav-bar/Navigation-bar.js
+++ src/components/Nav-bar/Navigation-bar.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./nav.module.scss";
-
+import logo from "../../logo.svg";
 function Navigation() {
   return (
-    <header>
-      <nav>
-        <ul className={styles.navi}>
-          <li>
+    <header className={styles.header}>
+      <div className={styles.headerlogo}>
+        <NavLink to="/">
+          <img className={styles.headerLogoIcon} src={logo} alt=""></img>
+        </NavLink>
+        <h1 className={styles.headerLogoText}>Stay Hard</h1>
+      </div>
+      <nav className={styles.nav}>
+        <ul className={styles.navitems}>
+          <li className={styles.navitem}>
             <NavLink to="/">Home</NavLink>
           </li>
-          <li>
+          <li className={styles.navitem}>
             <NavLink to="/About">About</NavLink>
           </li>
-          <li>
+          <li className={styles.navitem}>
             <NavLink to="/Contact">Contact</NavLink>
           </li>
-          <li>
+          <li className={styles.navitem}>
             <NavLink to="/RoadMap">Roadmap</NavLink>
+          </li>
+          <li className={styles.navitem}>
+            <button>Start</button>
           </li>
         </ul>
       </nav>
