ee58425c1cfc76c7bcd03b676e6817ef69580d0b
SandovaI <calebsandoval901@gmail.com>
2022-03-08 23:56:26 -0800

    background+vector

--- src/App.js
+++ src/App.js
@@ -5,35 +5,43 @@ import WalletConnection from "./components/Wallet/WalletConnection";
 
 function App() {
   return (
-    <header className={styles.header}>
-      <div className={styles.headerlogo}>
-        <NavLink to="/">
-          <img className={styles.headerLogoIcon} src={logo} alt=""></img>
-        </NavLink>
-        <h1 className={styles.headerLogoText}>Achieve It</h1>
-        <div className={styles.isConnected}></div>
-      </div>
+    <body className={styles.body}>
+      <header className={styles.header}>
+        <div className={styles.headerlogo}>
+          <NavLink to="/">
+            <img className={styles.headerLogoIcon} src={logo} alt=""></img>
+          </NavLink>
+          <h1 className={styles.headerLogoText}>Achieve It</h1>
+          <div className={styles.isConnected}></div>
+        </div>
 
-      <nav className={styles.nav}>
-        <ul className={styles.navitems}>
-          <li className={styles.navitem}>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li className={styles.navitem}>
-            <NavLink to="/About">About</NavLink>
-          </li>
-          <li className={styles.navitem}>
-            <NavLink to="/Contact">Contact</NavLink>
-          </li>
-          <li className={styles.navitem}>
-            <NavLink to="/RoadMap">Roadmap</NavLink>
-          </li>
-          <li className={styles.navitem}>
-            <WalletConnection />
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={styles.nav}>
+          <ul className={styles.navitems}>
+            <li className={styles.navitem}>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className={styles.navitem}>
+              <NavLink to="/About">About</NavLink>
+            </li>
+            <li className={styles.navitem}>
+              <NavLink to="/Contact">Contact</NavLink>
+            </li>
+            <li className={styles.navitem}>
+              <NavLink to="/RoadMap">Roadmap</NavLink>
+            </li>
+            <li className={styles.navitem}>
+              <WalletConnection />
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.mainLogo}>
+          <h1 className={styles.left}>Achieve</h1>
+          <h1 className={styles.right}>IT</h1>
+        </div>
+      </main>
+    </body>
   );
 }
 
